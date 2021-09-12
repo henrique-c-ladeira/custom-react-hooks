5a268f26100b19a6aa69ffe90706861adfa38587
Henrique <henrique.c.ladeira@gmail.com>
2021-09-12 14:12:16 -0300

    ✨ feat: App landing screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 function App() {
   return (
+    <>
     <h1>
       Custom hooks
     </h1>
+    <p> Check out 'src/hooks' to use the hooks. 
+      Currently there is no preview available</p>
+    </>
   );
 }
 
